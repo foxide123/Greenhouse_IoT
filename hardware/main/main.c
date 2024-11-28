dac10f07a03f59a4550a94935381cbd2ddfd9efe
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2024-11-28 15:13:23 +0100

    Adding code to main to initialize components , read data and construct JSON

--- hardware/main/main.c
+++ hardware/main/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "server.h"
 #include "temperature.h"
-#include "light_actuator"
-#include "light_sensor"
+#include "light_sensor.h"
+#include "light_actuator.h"
+//#include "light_sensor.h"
 
 
 void systemSetup(void);
@@ -14,7 +16,14 @@ int main(void)
 
 void systemSetup(void)
 {
-    char* temp_hum_data = temp_init();
-    light_init();
-    
+    float temperature, humidity, adc;
+
+    temp_init(&temperature, &humidity);
+    light_sensor_init(&adc);
+    light_actuator_init(50);
+
+    char* json_data = malloc(256);
+    snprintf(json_data, 256, "{\"temperature\":%.2f, \"humidity\":%.2f, \"adc\": %.0f}", temperature, humidity, adc);
+
+    printf("%s\n", json_data);
 }
